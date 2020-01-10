be884f57db473e6aa77a73335aad9fb2c8cef0d2
SungJin Lee <sjl@poscoict.com>
2020-01-10 15:59:10 +0900

    20200110

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from indeed import get_jobs as get_indeed_jobs
 from so import get_jobs as get_so_jobs
 from save_csv import save_to_file
 
-#indeed_jobs = get_indeed_jobs()
-#so_jobs = get_so_jobs()
-jobs = 'indeed_jobs'
+indeed_jobs = get_indeed_jobs()
+so_jobs = get_so_jobs()
+jobs = indeed_jobs + so_jobs
 save_to_file(jobs)
 
